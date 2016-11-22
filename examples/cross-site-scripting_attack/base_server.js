97b4d34550a498808f407600f9bfd110d138120f
Jimmy Tsai <jimmy_tsai@trend.com.tw>
2016-11-22 14:53:19 +0800

    Add sanitizer

--- examples/cross-site-scripting_attack/base_server.js
+++ examples/cross-site-scripting_attack/base_server.js
@@ -1,6 +1,7 @@
 "use strict";
 var express = require('express');
 var bodyParser = require('body-parser');
+var sanitizer = require('sanitizer')
 var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -36,7 +37,9 @@ app.get('/', function(req, res, next) {
 );
 
 app.post('/login', function(req, res, next) {
-    console.log(req.body);
+    console.log('raw body: ', req.body);
+    console.log('sanitized name: ', sanitizer.sanitize(req.body.name));
+    console.log('escaped name: ', sanitizer.escape(req.body.name));
     res.send({'abc':'abc'});
   }
 );
